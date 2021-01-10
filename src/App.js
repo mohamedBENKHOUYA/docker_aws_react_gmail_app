bbf3b801deca58ccd274a1c0edbefa1135ca595d
mohamed <mohamed.benkhouya.1995@gmail.com>
2021-01-10 23:23:59 +0100

    first cimmit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Header from './Header.js'
 import './App.css';
+import Sidebar from './Sidebar.js';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import Mail from "./Mail.js";
+import EmailList from './EmailList.js';
+import SendMail from "./SendMail";
+import {useSelector} from "react-redux";
+import {selectSendMessageIsOpen} from "./features/mailSlice";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    const sendMessageIsOpen = useSelector(selectSendMessageIsOpen)
+    return (
+        <Router>
+            <div>
+                <Header/>
+
+               <div className="app__body">
+                         <Sidebar />
+                    <Switch  >
+                        <Route path={"/mail"}>
+                            <Mail />
+                        </Route>
+                        <Route exact path={"/"}>
+                            <EmailList />
+                        </Route>
+                    </Switch>
+               </div>
+                {sendMessageIsOpen && <SendMail />}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Sidebar.css';
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SidebarOption from "./SidebarOption"
+import StartIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import {IconButton} from "@material-ui/core";
+import PersonIco from "@material-ui/icons/Person";
+import DuoIcon from "@material-ui/icons/Duo";
+import PhoneIcon from "@material-ui/icons/Phone";
+import {useDispatch} from "react-redux";
+import {openSendMessage} from "./features/mailSlice";
+
+const Sidebar = () => {
+    const dispatch = useDispatch();
+    return (
+        <div className={"sidebar"}>
+            <Button startIcon={
+                <AddIcon fontSize={"large"} />}  className={"sidebar__compose"}
+                            onClick={() => dispatch(openSendMessage())}>
+                 Compose
+            </Button>
+
+            <SidebarOption Icon={InboxIcon} title={"Inbox"} number={54} selected={false}/>
+            <SidebarOption Icon={StartIcon} title={"Starred"} number={54} selected={true}/>
+            <SidebarOption Icon={AccessTimeIcon} title={"Snoozed"} number={54}  selected={false}/>
+            <SidebarOption Icon={LabelImportantIcon} title={"Important"} number={54}  selected={false}/>
+            <SidebarOption Icon={NearMeIcon} title={"Sent"} number={54}  selected={false}/>
+            <SidebarOption Icon={NoteIcon} title={"Drafts"} number={54}  selected={false}/>
+            <SidebarOption Icon={ExpandMoreIcon} title={"More"} number={54}  selected={false}/>
+
+            <div className="sidebar__footer">
+                <div className="sidebar__footerIcons">
+                    <IconButton>
+                        <PersonIco />
+                    </IconButton>
+                    <IconButton>
+                        <DuoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <PhoneIcon />
+                    </IconButton>
+
+                </div>
+            </div>
+        </div>
+    );
+
+
+}
+
+export default Sidebar;
